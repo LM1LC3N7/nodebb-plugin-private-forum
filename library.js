205d04796e962e41c266c31799a8f87990ed04de
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2020-06-03 12:35:35 +0200

    test

--- library.js
+++ library.js
@@ -1,31 +1,18 @@
 'use strict';
 
-const controllers = require('./lib/controllers');
-
 const plugin = {};
 
-plugin.init = function (params, callback) {
-	const router = params.router;
-	const hostMiddleware = params.middleware;
-	// const hostControllers = params.controllers;
-
-	// We create two routes for every view. One API call, and the actual route itself.
-	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
-
-	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/quickstart', controllers.renderAdminPage);
-
+plugin.init = function (app, params, callback) {
+	var helpers = require.main.require('./src/routes/helpers');
+	
+	if ((req.url !== "/login") || (req.url !== "/register")) {
+		helpers.notAllowed(app.req, app.res, function(error){
+			// TODO
+			if (error) return next(err);
+		});
+	}
 	callback();
-};
-
-plugin.addAdminNavigation = function (header, callback) {
-	header.plugins.push({
-		route: '/plugins/quickstart',
-		icon: 'fa-tint',
-		name: 'Quickstart',
-	});
-
-	callback(null, header);
+});
 };
 
 module.exports = plugin;
