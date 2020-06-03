55bb6e60aac8782d286cf30d66b6980b21eeb1ef
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2020-06-03 21:49:55 +0200

    First working version!

--- library.js
+++ library.js
@@ -10,19 +10,25 @@ plugin.init = function (params, callback) {
 	// ADD A MIDDLEWARE AND GET "req" object
 
 	const { app, middleware, router } = params;
-	var allowedPages=["/login", "/register", "/reset"];
-	
+	// var allowedPages=["/login", "/register", "/reset"];
+	let allowedPages = /\/(assets\/|login|register|reset|plugins\/).*/;
+
+
 	console.log("==================================");
 	console.log(" Plugin Private Forum Initialized ");
 	console.log("==================================");
-	
-	if (allowedPages.indexOf(req.url) < 0) {
-		helpers.notAllowed(req, res, next);
-	} else {
-		console.log("PLUGIN PRIVATE FORUM: req.url="+req.url+", user is logged");
-		winston.log("PLUGIN PRIVATE FORUM: req.url="+req.url+", user is logged");
-		next();
-	}
+
+	app.use(function (req, res, next) {
+		// if ((allowedPages.indexOf(req.url) > 0) || (assetsPages.test(req.url)) || (req.uid === 0)) {
+		if ((allowedPages.test(req.url)) || (req.uid !== 0)) {
+			console.log("nodebb-plugin-private-forum:DEBUG: user is logged and / or URL = "+ req.url +", no redirect.");
+			next();
+		} else {
+			console.log("nodebb-plugin-private-forum:DEBUG: User is not logged and URL = "+ req.url +", redirecting to login page.");
+			helpers.notAllowed(req, res, next);
+		}
+	});
+	callback();
 };
 
 module.exports = plugin;
