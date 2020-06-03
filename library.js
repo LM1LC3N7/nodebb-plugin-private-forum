ffac0595d1364b73a7f4bb70081c01c584ba0619
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2020-06-03 15:44:59 +0200

    Rename the plugin & dev version updated

--- library.js
+++ library.js
@@ -2,17 +2,17 @@
 
 const plugin = {};
 
+// WE NEED A MIDDLEWARE HERE
+//
 plugin.init = function (app, params, callback) {
 	var helpers = require.main.require('./src/routes/helpers');
-	
+	var req = app.req;	
 	if ((req.url !== "/login") || (req.url !== "/register")) {
 		helpers.notAllowed(app.req, app.res, function(error){
-			// TODO
 			if (error) return next(err);
 		});
 	}
 	callback();
-});
 };
 
 module.exports = plugin;
