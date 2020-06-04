78855d21654a43f668fa6dfe40f42666fcaffc18
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2020-06-04 11:24:39 +0200

    Remove debug code for v1.0.0 pre-release

--- library.js
+++ library.js
@@ -6,21 +6,11 @@ const helpers = require.main.require('./src/controllers/helpers');
 
 plugin.init = function (params, callback) {
 	
-	// TODO
-	// ADD A MIDDLEWARE AND GET "req" object
-
 	const { app, middleware, router } = params;
-	// var allowedPages=["/login", "/register", "/reset"];
 	let allowedPages = /\/(assets\/.*|login|register|reset|plugins\/.*)/;
 
-
-	console.log("==================================");
-	console.log(" Plugin Private Forum Initialized ");
-	console.log("==================================");
-
 	app.use(function (req, res, next) {
-		// if ((allowedPages.indexOf(req.url) > 0) || (assetsPages.test(req.url)) || (req.uid === 0)) {
-		if ((allowedPages.test(req.url)) || (req.url === "/") || (req.uid !== 0)) {
+		if ((allowedPages.test(req.url)) || (req.url === "/") || (req.uid > 0)) {
 			winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
 			next();
 		} else {
