7281989f496341ef92af850fab8f197b2eb9c89c
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2021-04-13 23:44:12 +0200

    Update library

--- library.js
+++ library.js
@@ -1,24 +1,20 @@
-'use strict';
-
 const plugin = {};
-var winston = module.parent.require('winston');
+let winston = module.parent.require('winston');
 const helpers = require.main.require('./src/controllers/helpers');
 
-plugin.init = function (params, callback) {
-	
-	const { app, middleware, router } = params;
-	let allowedPages = /\/(assets\/|login|register|reset|plugins\/).*|.*(.css|.js)$/;
-	
-	router.use(function (req, res, next) {
-		if ((allowedPages.test(req.url)) || (req.url === "/") || (req.uid > 0)) {
-			winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
-			next();
-		} else {
-			winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ req.url +"), redirecting to login page.");
-			helpers.notAllowed(req, res, next);
-		}
-	});
-	callback();
+plugin.init = async (params, callback) => {
+
+        const { app, middleware, router } = params;
+        let allowedPages = /\/(assets\/|login|register|reset|plugins\/).*|.*(.css|.js)$/;
+
+        router.use(function (req, res, next) {
+                if (req.loggedIn || allowedPages.test(req.url)) {
+                        winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
+                        return next();
+                }
+                winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ req.url +"), redirecting to login page.");
+                return helpers.notAllowed(req, res, next);
+        });
 };
 
 module.exports = plugin;
