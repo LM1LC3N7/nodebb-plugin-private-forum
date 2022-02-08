b38167d6d95a7e62572eb824fd196cf15d1218d9
Louis MILCENT <14235706+LM1LC3N7@users.noreply.github.com>
2022-02-08 15:07:47 +0100

    Update library.js

--- library.js
+++ library.js
@@ -2,20 +2,18 @@ const plugin = {};
 let winston = module.parent.require('winston');
 const helpers = require.main.require('./src/controllers/helpers');
 
-plugin.init = async (params, callback) => {
-
-        const { app, middleware, router } = params;
+plugin.privateforum = async (data, callback) => {
+      
+        var req = data.req;
+        var res = data.res;
         let allowedPages = /\/(assets\/|login|register|reset|auth|plugins\/).*|.*(.css|.js)$/;
 
-        router.use(function (req, res, next) {
-                if (req.loggedIn || allowedPages.test(req.url)) {
-                        winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
-                        return next();
-                } else {
-                        winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ req.url +"), redirecting to login page.");
-                        return helpers.notAllowed(req, res, next);
-                }
-        });
+        if (req.loggedIn || allowedPages.test(req.url)) {
+                winston.verbose("[plugin-nodebb-private-forum] User is logged or URL is allowed ("+ req.url +"), no redirect.");
+        } else {
+                winston.verbose("[plugin-nodebb-private-forum] User is not logged and URL is not allowed ("+ req.url +"), redirecting to login page.");
+                helpers.notAllowed(req, res);
+        }
 };
 
 module.exports = plugin;
